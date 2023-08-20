cbf4820ccbad79e61201d9caf3d12f449b06af2b
Rivky Shoham <142743496+ritsho@users.noreply.github.com>
2023-08-20 21:25:53 +0300

    second page - start

---
+++ loginpage.js
@@ -0,0 +1,9 @@
+function login(){
+    // get username from HTML
+    let username = document.getElementById("username").value;
+    // get PW from HTML
+    let password = document.getElementById("password").value;
+
+    // check if this user has the correct pw.
+    
+}
