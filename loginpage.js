0347d5935f055d2e4fb074649e746e3c14eee8d7
Rivky Shoham <142743496+ritsho@users.noreply.github.com>
2023-08-22 23:03:32 +0300

    login page is ready

--- loginpage.js
+++ loginpage.js
@@ -1,9 +1,53 @@
-function login(){
-    // get username from HTML
+let users_list = [];
+let pwd_list = [];
+
+function login() {
+
+    // let users = [];
+    // let pwds = [];
+    // users.push("debug");
+    // pwds.push("debug");
+    // localStorage.setItem("users_list",JSON.stringify(users));
+    // localStorage.setItem("pwd_list",JSON.stringify(pwds));
+
+    // להשיג את רשימת המשתמשים והסיסמאות
+    users_list = JSON.parse(localStorage.getItem("users_list"));
+    pwd_list = JSON.parse(localStorage.getItem("pwd_list"));
+
+
     let username = document.getElementById("username").value;
-    // get PW from HTML
     let password = document.getElementById("password").value;
 
-    // check if this user has the correct pw.
-    
+    // בדיקה אם לא כתבו שם משתמש או סיסמה
+    if (username == "") {
+        alert("נא למלא שם משתמש");
+    }
+    else if (password == "") {
+        alert("נא למלא סיסמה");
+    }
+    // אם המשתמש לא קיים ברשימה
+    else if (!users_list.includes(username)) {
+        alert("שם המשתמש לא קיים");
+    }
+    // שם המשתמש קיים, לבדוק את הסיסמה
+    else {
+        // להשיג את האינדקס של המשתמש 
+        let user_index = users_list.indexOf(username);
+        let real_pwd = pwd_list[user_index];
+
+        if (password == real_pwd) {
+            // לשמור מי המשתמש שנכנס למערכת
+            localStorage.setItem("active_user", username);
+
+            // לפתוח את העמוד הראשי
+            window.location.href = "home-page.html";
+        } else {
+            alert("סיסמה שגויה");
+        }
+    }
+}
+
+function signup() {
+    // לעבור לעמוד הרישום
+    window.location.href = "register-update.html";
 }
---
+++ shifts-classes.js
@@ -0,0 +1,63 @@
+class Employee {
+    constructor(username, password, firstName, lastName, yearOfBirth) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+        this.cvWorks = [];
+        this.cvEducations = []
+        this.shifts = [];
+    }
+
+    CheckDetails() {
+        // לבדוק שכל הנתונים תקינים
+    }
+
+    Save() {
+        // לשמור את הנתונים ל
+        // Local storage
+    }
+}
+
+class CV_Work {
+    constructor(startYear, endYear, bossName, jobName) {
+        this.startYear = startYear;
+        this.endYear = endYear;
+        this.bossName = bossName;
+        this.jobName = jobName;
+    }
+
+    CheckDetails() {
+        // לבדוק שכל הנתונים תקינים
+    }
+
+}
+
+class CV_Education {
+    constructor(startYear, endYear, mosadName, educationName) {
+        this.startYear = startYear;
+        this.endYear = endYear;
+        this.mosadName = mosadName;
+        this.educationName = educationName;
+    }
+    CheckDetails() {
+        // לבדוק שכל הנתונים תקינים
+    }
+
+}
+
+class Shifts {
+    constructor(date, startHour, endHour, hourPayment, jobType, branch) {
+        this.date = date;
+        this.startHour = startHour;
+        this.endHour = endHour;
+        this.hourPayment = hourPayment;
+        this.jobType = jobType;
+        this.branch = branch;
+    }
+
+    GetTotalPayment() {
+        // לחשב כמה משכורת ירוויח העובד מהמשמרת הנוכחית
+    }
+}
