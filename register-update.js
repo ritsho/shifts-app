f379615d919793906ef07a2cb50b3a9e2e26aed7
Rivky Shoham <142743496+ritsho@users.noreply.github.com>
2023-08-23 00:26:36 +0300

    register page start

--- register-update.js
+++ register-update.js
@@ -1,1 +1,21 @@
 
+function register(){
+    // להשיג את כל הנתונים מה 
+    //HTML
+    let email = document.getElementById("email").value;
+    let username = document.getElementById("username").value;
+    let password1 = document.getElementById("password1").value;
+    let password2 = document.getElementById("password2").value;
+    let firstname = document.getElementById("firstname").value;
+    let lastname = document.getElementById("lastname").value;
+    let age = parseInt(document.getElementById("age").value);
+
+
+    // בדיקת תקינות 
+    if (!email.contains("@") || !email.contains(".")){
+        alert("מייל לא תקין");
+    }
+    // שמירה
+
+    // מעבר לעמוד הבית
+}
